6037232e7f0e9201a7f23b9b97d8eee14166634a
Your Name <you@example.com>
2018-07-31 20:37:13 +0700

    first commit

---
+++ clients/client.js
@@ -0,0 +1,59 @@
+const request = require('request');
+const fs = require('fs');
+
+const path = require('path');
+const express = require('express');
+const http = require('http');
+// var app = express();
+
+// app.set('port', process.env.PORT || 3001);
+
+// http.createServer(app).listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+// File .txt
+var target = 'http://localhost:3000/input.txt' ;//+ path.String(basename(filename));
+var rs = fs.createReadStream('input.txt');
+var ws = request.post(target);
+// File .snp
+var target2 = 'http://localhost:3000/client.snp' ;
+var rs = fs.createReadStream('client.snp');
+var ws = request.post(target2);
+
+ws.on('drain', function () {
+  console.log('drain', new Date());
+  rs.resume();
+});
+
+rs.on('end', function () {
+  console.log('uploaded to ' + target);
+});
+
+ws.on('error', function (err) {
+  console.error('cannot send file to ' + target + ': ' + err);
+});
+
+rs.pipe(ws);
+
+var app = express();
+app.set('port', process.env.PORT || 3001);
+app.post('/:filename', function (req, res) {
+  var filename = path.basename(req.params.filename);
+  filename = path.resolve(__dirname, filename);
+  var dst = fs.createWriteStream(filename);
+  console.log('Received from server!');
+  req.pipe(dst);
+
+  dst.on('drain', function() {
+    console.log('drain', new Date());
+    req.resume();
+  });
+  req.on('end', function () {
+    res.sendStatus(200);
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+var request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.post('/:filename', function (req, res) {
+  var filename = path.basename(req.params.filename);
+  filename = path.resolve(__dirname, filename);
+  var dst = fs.createWriteStream(filename);
+  console.log('Received input!');
+  req.pipe(dst);
+
+  dst.on('drain', function () {
+    console.log('drain', new Date());
+    req.resume();
+  });
+  req.on('end', function () {
+    // to do
+    setTimeout(() => {
+      // File ckt_list.txt
+      var target = 'http://localhost:3001/ckt_list.txt';
+      var rs = fs.createReadStream('ckt_list.txt');
+      var ws = request.post(target);
+      // File arrays
+      var target2 = 'http://localhost:3001/arrays.txt';
+      var rs = fs.createReadStream('arrays.txt');
+      var ws = request.post(target2);
+
+      ws.on('drain', function () {
+        console.log('drain', new Date());
+        rs.resume();
+      });
+
+      rs.on('end', function () {
+        console.log('uploaded to ' + target);
+      });
+
+      ws.on('error', function (err) {
+        console.error('cannot send file to ' + target + ': ' + err);
+      });
+
+      rs.pipe(ws);
+      res.sendStatus(200);
+    },
+      3000)
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
